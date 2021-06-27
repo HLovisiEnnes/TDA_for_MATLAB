c236900505543505859b0067b93402a47f35b659
H Ennes <hlovisiennes@gmail.com>
2021-06-26 21:01:00 -0300

    Update distance_matrix.m

--- distance_matrix.m
+++ distance_matrix.m
@@ -1,8 +1,11 @@
 function distance_matrix(x, bins)
+size_x = size(x);
 dist = [];
-    for y = transpose(x)
-        for z = transpose(x)
-            dist = [dist, sqrt(dot(z-y, z-y))];
+    for y = 1:size_x(1)
+        for z = 1:size_x(1)
+            if x(z,:) ~= x(y,:) 
+                dist = [dist, sqrt(dot(x(z,:) - x(y,:), x(z,:) - x(y,:)))];
+            end
         end
     end
 histogram(dist, bins)
